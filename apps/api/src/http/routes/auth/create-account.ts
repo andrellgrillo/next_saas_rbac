0278034120aa98cbdd8c287070e0c7aec284f8e3
Andre Luiz Leite Grillo <andrell.grillo@gmail.com>
2024-06-20 11:35:23 -0300

    setup app com fastfy

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,17 @@
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import type { FastifyInstance } from "fastify";
+import { z } from 'zod'
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post('/users',{
+    schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6)
+      })
+    }
+  }, () => {
+    return 'user created!'
+  })
+}
