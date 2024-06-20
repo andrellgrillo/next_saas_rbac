11cd423d6b6a773fcec268ad04e19da5ce7e6f8d
Andre Luiz Leite Grillo <andrell.grillo@gmail.com>
2024-06-20 17:37:01 -0300

    seeds

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import type { FastifyInstance } from "fastify";
 import { z } from 'zod'
+import { hash } from "bcryptjs";
+import { prisma } from "@/lib/prisma";
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/users',{
@@ -11,7 +13,22 @@ export async function createAccount(app: FastifyInstance) {
         password: z.string().min(6)
       })
     }
-  }, () => {
-    return 'user created!'
+  }, async (request, reply) => {
+    const { name, email, password } = request.body
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: { email }
+    })
+    if(userWithSameEmail) {
+      return reply.status(400).send({ message: 'user with same e-mail already exists.'})
+    }
+    const passwordHash = await hash(password, 6)
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash
+      },
+    })
+    return reply.status(201).send()
   })
 }
