0323966d7c9238fdd56cbbc8b58eac13a23c63df
Andre Grillo <andrell.grillo@gmail.com>
2025-02-03 15:43:58 -0300

    teste

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -7,33 +7,39 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" type="name" id="name" />
+      </div>
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
       </div>
       <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
+        <Label htmlFor="password_confirmation">Password</Label>
+        <Input
+          name="password_confirmation"
+          type="password"
+          id="password_confirmation"
+        />
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password">Confirm your password</Label>
         <Input name="password" type="password" id="password" />
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
       </div>
       <Button type="submit" className="w-full">
-        Sign in with e-mail
+        Create account
       </Button>
       <Button variant="link" className="w-full" asChild>
-        <Link href="/auth/sign-up">Create new account</Link>
+        <Link href="/auth/sign-in">Already registered? Sing in</Link>
       </Button>
       <Separator />
       <Button type="submit" variant="outline" className="w-full">
         <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
-        Sign in with Github
+        Sign up with Github
       </Button>
     </form>
   )
