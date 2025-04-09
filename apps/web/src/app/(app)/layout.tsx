a061190600dd4a8a52557e4ddda5b1d45d1114fa
Andre Grillo <andrell.grillo@gmail.com>
2025-04-09 04:57:27 -0300

    feat: header component and profile buton

---
+++ apps/web/src/app/(app)/layout.tsx
@@ -0,0 +1,12 @@
+import { redirect } from 'next/navigation'
+
+import { isAuthenticated } from '@/auth/auth'
+
+export default async function AppLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  if (!isAuthenticated()) {
+    redirect('/auth/sign-in')
+  }
+  return <>{children}</>
+}
