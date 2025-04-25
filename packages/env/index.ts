0dc65942b4f68cb9f7191d34af0b799b6161286b
Andre Grillo <andrell.grillo@gmail.com>
2025-04-25 16:15:11 -0300

    chord: add variavel de ambiente

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -11,8 +11,11 @@ export const env = createEnv({
     GITHUB_OAUTH_CLIENT_REDIRECT_URI: z.string().url(),
   },
   client: {},
-  shared: {},
+  shared: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+  },
   runtimeEnv: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     SERVER_PORT: process.env.SERVER_PORT,
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
