589436447805267429f582bafefad51614bb3596
Andre Grillo <andrell.grillo@gmail.com>
2025-09-18 10:42:52 -0300

    update para atualizar projeto em outro pc

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -2,9 +2,9 @@ import { Header } from '@/components/header'
 
 export default async function Project() {
   return (
-    <div className="py-4">
+    <div className="space-y-4 py-4">
       <Header />
-      <main className="mx-auto w-full max-w-[1200px]space-y-4">Projects</main>
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">Projects</main>
     </div>
   )
 }
